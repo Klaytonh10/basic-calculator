9abed127dae12a95b58de78c2748f3eaea960aa0
Klaytonh10 <klaytonh10@gmail.com>
2025-09-25 10:47:40 -0500

    Made basic calculator

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,7 +1,37 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input first number(int): ");
+        int firstNumber = scanner.nextInt();
+
+
+        System.out.print("Input second number(int): ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.print("+, -, /, or *: ");
+        String operator = scanner.next();
+
+        if(operator.equals("+")) {
+            System.out.println("Result: " + (firstNumber + secondNumber));
+        } else if(operator.equals("-")) {
+            System.out.println("Result: " + (firstNumber - secondNumber));
+        } else if(operator.equals("/")) {
+            if(secondNumber != 0) {
+                System.out.println("Result: " + (firstNumber / secondNumber));
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+            }
+        } else if(operator.equals("*")) {
+            System.out.println("Result: " + (firstNumber * secondNumber));
+        } else {
+            System.out.println("Error: Invalid operator.");
+        }
 
     }
 }
